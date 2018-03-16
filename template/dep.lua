b08098736957d6c53ef25eb272e10bfc6fc321ed
igorcoding <igorcoding@users.noreply.github.com>
2018-03-16 18:30:02 +0300

    dep.lua fixes

--- template/dep.lua
+++ template/dep.lua
@@ -1,5 +1,7 @@
 #! /usr/bin/env tarantool
 
+assert(_TARANTOOL ~= nil, 'dep.lua must be run by tarantool')
+
 local fio = require 'fio'
 local yaml = require 'yaml'
 
@@ -11,13 +13,37 @@ local function fprint(f, ...)
 end
 
 
+local function _is_dir(path)
+    if _TARANTOOL >= "1.9" then
+        return fio.path.is_dir(path)
+    end
+
+    local fs = fio.stat(path)
+    return fs ~= nil and fs:is_dir() or false
+end
+
+local function _exists(path)
+    if _TARANTOOL >= "1.9" then
+        return fio.path.exists(path)
+    end
+    return fio.stat(path) ~= nil
+end
+
+local function _string_split(s)
+    local parts = {}
+    for word in string.gmatch(s, '([^,]+)') do
+        table.insert(parts, word)
+    end
+    return parts
+end
+
 local function ensure_rocksservers(path)
     local dir = fio.dirname(path)
-    if not fio.path.is_dir(dir) then
+    if not _is_dir(dir) then
         fio.mkdir(dir)
     end
 
-    if fio.path.exists(path) then
+    if _exists(path) then
         local data = fio.open(path):read()
         if data:match('rocks%.tarantool%.org') then
             fprint('Already have rocks.tarantool.org')
@@ -25,7 +51,7 @@ local function ensure_rocksservers(path)
         end
     end
     fprint("Patch %s with proper rocks servers", path)
-    local fh = fio.open(path, {'O_APPEND', 'O_RDWR'})
+    local fh = fio.open(path, {'O_CREAT', 'O_APPEND', 'O_RDWR'})
     fh:write('\nrocks_servers = {[[http://rocks.tarantool.org/]], [[https://rocks.moonscript.org]]}\n')
     fh:close()
 end
@@ -75,14 +101,17 @@ local tarantoolctl_install = _gencmd(cmd_tarantoolctl, 'install')
 
 
 local function main()
+    print('Tarantool version: ' .. _TARANTOOL)
+
     local luaroot = debug.getinfo(1, 'S')
     local source = fio.abspath(luaroot.source:match('^@(.+)'))
     local appname = fio.basename(fio.dirname(source))
 
     local args = {
         ['--luarocks-config'] = fio.pathjoin(os.getenv('HOME'), '.luarocks', 'config.lua'),
-        ['--meta-file']       = '',
+        ['--meta-file']       = './meta.yaml',
         ['--tree']            = '.rocks',
+        ['--only']            = '',
     }
 
     for i = 1,#arg/2 do args[ arg[i*2-1] ] = arg[i*2] end
@@ -92,9 +121,16 @@ local function main()
 
     print('Using the following options:\n' .. yaml.encode(args))
 
-    local meta_file = fio.abspath(meta_path)
-    local metatext = fio.open(meta_file):read()
+    local meta_file = fio.open(fio.abspath(meta_path))
+    local metatext = meta_file:read(meta_file:stat().size)
     local tree = fio.abspath(args['--tree'])
+    local only_sections
+    if args['--only'] ~= '' then
+        only_sections = {}
+        for _, s in ipairs(_string_split(args['--only'])) do
+            only_sections[s] = true
+        end
+    end
 
     cfg = metatext:match('^%s*%{') and require 'json'.decode(metatext) or yaml.decode(metatext)
 
@@ -108,37 +144,43 @@ local function main()
     local tnt_deps = cfg.tnt_deps or cfg.tntdeps or {}
     local local_deps = cfg.local_deps or cfg.localdeps or {}
 
-    for _, dep in ipairs(deps) do
-        fprint("Installing dep '%s'", dep)
-        luarocks_install(dep, tree)
-        fprint("Installed dep '%s'\n\n", dep)
-    end
-
-    for _, dep in ipairs(tnt_deps) do
-        fprint("Installing tarantool dep '%s'", dep)
-        tarantoolctl_install(dep, tree)
-        fprint("Installed tarantool dep '%s'\n\n", dep)
-    end
-
-    for _, dep in ipairs(local_deps) do
-        fprint("Installing local dep '%s'", dep)
-        dep = fio.abspath(dep)
-        dep_path = fio.dirname(dep)
-
-        cwd = fio.cwd()
-        fio.chdir(dep_path)
-        local ok, res = pcall(luarocks_remove, dep, tree)
-        if not ok then
-            fprint(res)
+    if only_sections == nil or only_sections['deps'] then
+        for _, dep in ipairs(deps) do
+            fprint("Installing dep '%s'", dep)
+            luarocks_install(dep, tree)
+            fprint("Installed dep '%s'\n\n", dep)
         end
+    end
 
-        luarocks_make(dep, tree)
-        fio.chdir(cwd)
-        fprint("Installed local dep '%s'\n\n", dep)
+    if only_sections == nil or only_sections['tntdeps'] or only_sections['tnt_deps'] then
+        for _, dep in ipairs(tnt_deps) do
+            fprint("Installing tarantool dep '%s'", dep)
+            tarantoolctl_install(dep, tree)
+            fprint("Installed tarantool dep '%s'\n\n", dep)
+        end
+    end
+
+    if only_sections == nil or only_sections['localdeps'] or only_sections['local_deps'] then
+        local cwd = fio.cwd()
+        for _, dep in ipairs(local_deps) do
+            fprint("Installing local dep '%s'", dep)
+            dep = fio.abspath(dep)
+
+            fio.chdir(fio.dirname(dep)) -- local rocks must be installed from within the project root
+            local ok, res = pcall(luarocks_remove, dep, tree)
+            if not ok then
+                fprint(res)
+            end
+
+            luarocks_make(dep, tree)
+            fio.chdir(cwd)
+            fprint("Installed local dep '%s'\n\n", dep)
+        end
     end
     fprint('Done.')
 end
 
 xpcall(main, function(err)
     print(err .. '\n' .. debug.traceback())
+    os.exit(1)
 end)
